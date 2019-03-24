e704b8820053b889f831092ad503ba98bc448936
meowcodes <meowcodes@gmail.com>
2019-03-24 12:22:13 -0700

    update styling

--- src/MyList.js
+++ src/MyList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import './MyList.css';
+import './MyList.css';
 
 /**
  * Receives list from App
@@ -27,19 +27,21 @@ class MyList extends Component {
             <div className="MyList">
                 <div className="MyList-show">
                     <div className="MyList-share">
-                        <h3 className="MyList-title">Share with Friends</h3>
-
-<p>This prototype provides an introduction we wrote plus your list: copy, paste, rewrite any way you like, and share with you who you like.</p>         
+                        <h1 className="MyList-title">Share with Friends</h1>
+                        <p className="MyList-text">This prototype provides an introduction we wrote plus your list: copy, paste, rewrite any way you like, and share with you who you like.</p>         
                     </div>
-                    <hr/>          
-                    <p className="MyList-intro">Hey friends — Sometimes I go through tough times, and could really use some help. I believe my friends want to be there, and maybe don't know what I need, so I've written down a few ways that friends could help — most of them mutual. I would really feel gratitude if you can commit to partnering with me on just one of these:</p>
-                    <ul className="MyList-list">
-                        { this.props.list.map( item =>
-                            <li className="MyList-list-item" key={ item.id }>{ item.text }</li>
-                        )}
-                    </ul>
-                    <p>I wrote this post with some tips from http://alittlehelpfrom.org</p>
-                    <p>Check it out and consider creating your own.</p> 
+        
+                    <div className="MyList-share-box">
+                        <p className="MyList-intro"><span>Hey friends</span> — Sometimes I go through tough times, and could really use some help. I believe my friends want to be there, and maybe don't know what I need, so I've written down a few ways that friends could help — most of them mutual.</p> 
+                        <p>I would really feel gratitude if you can commit to partnering with me on just one of these:</p>
+                        <ul className="MyList-list">
+                            { this.props.list.map( item =>
+                                <li className="MyList-list-item" key={ item.id }>{ item.text }</li>
+                            )}
+                        </ul>
+                        <p>I wrote this post with some tips from <a href="http://alittlehelpfrom.org">http://alittlehelpfrom.org</a></p>
+                        <p>Check it out and consider creating your own.</p> 
+                        </div>
                 </div>
             </div>
         );
